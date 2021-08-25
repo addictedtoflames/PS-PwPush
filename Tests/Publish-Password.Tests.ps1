1a558c61eb8de3406fcbd52e58f9bcf08e35f53a
David Hollings <david@davidshomelab.com>
2021-08-25 16:44:24 +0100

    Test that warning is generated when receiving cleartext password

--- Tests/Publish-Password.Tests.ps1
+++ Tests/Publish-Password.Tests.ps1
@@ -50,6 +50,9 @@ Describe -Name "Publish-Password" {
     It "Accepts SecureString Password From New-Password" {
         (New-Password -AsSecureString | Publish-Password).Password | Should -Be $plaintextpassword
     }
+    It "Warns when publishing a cleartext password" {
+        {Publish-Password -Password $plaintextpassword -WarningAction Stop} | Should -Throw -ExpectedMessage "The running command stopped because the preference variable `"WarningPreference`" or common parameter is set to Stop: It is recommended to input the password as a secure string."
+    }
 
     
 }
